817a3c662ec03c86fb71293ff780a5a25b672df3
Spencer Peck <speck34@bruinmail.com>
2026-02-15 14:29:52 -0700

    Added Driver Code. Made some commenting in Sorter.

--- project1CS2430/src/project1CS2430/Main.java
+++ project1CS2430/src/project1CS2430/Main.java
@@ -1,9 +1,44 @@
 package project1CS2430;
 
+/*
+ * Runs four algorithms on each permutation, and records:
+ * Algorithm name
+ * The unsorted array used
+ * Number of comparisons
+ */
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Change this to adjust how large the arrays to be sorted are.
+		 *Assignment requires to gather data on: (dataLength)n = 4, 6, 8
+		 */ 
+		int dataLength = 4;
+		
+		int[] arry; //Stores the data to sort
+		
+		//Run Quick Sort
+		arry = ArrayGenerator.firstPermutation(dataLength); //Start with clean array
+		while(ArrayGenerator.findNextPermutation(arry)) {//Repeat until no new permutation 
+			Sorter.quickSort(arry);
+		}
+
+		//Run Shaker Sort
+		arry = ArrayGenerator.firstPermutation(dataLength); //Start with clean array
+		while(ArrayGenerator.findNextPermutation(arry)) {//Repeat until no new permutation 
+			Sorter.shakerSort(arry);
+		}
+		
+		//Run Heap Sort
+		arry = ArrayGenerator.firstPermutation(dataLength); //Start with clean array
+		while(ArrayGenerator.findNextPermutation(arry)) { //Repeat until no new permutation 
+			//Sorter.heapSort(arry, ?, ?);
+		}
+		
+		//Run Merge Sort
+		arry = ArrayGenerator.firstPermutation(dataLength); //Start with clean array
+		while(ArrayGenerator.findNextPermutation(arry)) {//Repeat until no new permutation 
+			//Sorter.mergeSort(arry);
+		}
 
 	}
 
